d197d5a7cf8a57992d924eadb0dea3b94983ce90
Ugur Pekunsal <ugurpekunsal@gmail.com>
2022-03-30 15:22:18 -0400

    Updated resume

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,47 +2,43 @@ import React, { Component } from "react";
 import Fade from "react-reveal";
 
 class Footer extends Component {
-  render() {
-    if (!this.props.data) return null;
+	render() {
+		if (!this.props.data) return null;
 
-    const networks = this.props.data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
+		const networks = this.props.data.social.map(function (network) {
+			return (
+				<li key={network.name}>
+					<a href={network.url}>
+						<i className={network.className}></i>
+					</a>
+				</li>
+			);
+		});
 
-    return (
-      <footer>
-        <div className="row">
-          <Fade bottom>
-            <div className="twelve columns">
-              <ul className="social-links">{networks}</ul>
+		return (
+			<footer>
+				<div className="row">
+					<Fade bottom>
+						<div className="twelve columns">
+							<ul className="social-links">{networks}</ul>
 
-              <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
-                <li>
-                  Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </Fade>
+							<ul className="copyright">
+								<li>
+									&copy; Copyright 2020-2022 PKNSL Website Design & Development
+								</li>
+							</ul>
+						</div>
+					</Fade>
 
-          <div id="go-top">
-            <a className="smoothscroll" title="Back to Top" href="#home">
-              <i className="icon-up-open"></i>
-            </a>
-          </div>
-        </div>
-      </footer>
-    );
-  }
+					<div id="go-top">
+						<a className="smoothscroll" title="Back to Top" href="#home">
+							<i className="icon-up-open"></i>
+						</a>
+					</div>
+				</div>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
